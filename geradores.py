640232b878c23788ea23464dbe122d20f6868a65
Luiz Guilherme Silva <luizguilhermeprog@gmail.com>
2021-10-02 13:47:44 -0300

    Adjusting sort

--- geradores.py
+++ geradores.py
@@ -4,4 +4,6 @@ import random
 def gerador_mega_sena():
     numeros = list(range(1, 60))
     random.shuffle(numeros)
-    return numeros[:6]
+    numeros = numeros[:6]
+    numeros.sort()
+    return numeros
